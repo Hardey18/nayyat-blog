c43d59a7920fd9639f08749f7485bd8ab9faea94
Nurudeen Adewale <nurudeenadeolaa@gmail.com>
2021-11-15 14:47:26 +0100

    new clean up

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -5,38 +5,22 @@ import { getCategories, getCategoryPost } from '../../services';
 import { PostCard, Categories, Loader } from '../../components';
 
 const CategoryPost = ({ posts }) => {
-  // const [postCategory, setPostCategory] = useState("");
-  // useEffect(() => {
-    //   if (reversedData[0].node.categories.length > 1) {
-      //     setPostCategory(reversedData[0].node.categories[reversedData[0].node.categories.length - 1].name)
-      //   }
-      //   setPostCategory(reversedData[0].node.categories[0].name)
-      
-      // })
-      
-      const [postCategory, setPostCategory] = useState("");
-      let reversedData = [...posts].reverse();
-      useEffect(() => {
-        setPostCategory(posts[0].node.categories[0].name)
-      })
-      const router = useRouter();
-      
-      if (router.isFallback) {
-        return <Loader />;
-      }
-      const router = useRouter();
-      
-      if (router.isFallback) {
-        return <Loader />;
-      }
+  const [postCategory, setPostCategory] = useState("");
+  useEffect(() => {
+    setPostCategory(posts[0].node.categories[0].name)
+  })
+  const router = useRouter();
 
+  if (router.isFallback) {
+    return <Loader />;
+  }
 
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
         <div className="text-2xl text-white mb-8">{postCategory} posts</div>
-          {reversedData.map((post, index) => (
+          {posts.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
         </div>
