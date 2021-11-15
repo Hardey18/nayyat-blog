16233c8127db8abe9fbd5348743f3f8cee9274b7
Nurudeen Adewale <nurudeenadeolaa@gmail.com>
2021-11-15 15:22:15 +0100

    adjust padding in Postcard

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -20,7 +20,7 @@ const CategoryPost = ({ posts }) => {
   }
 
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto px-4 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
         <div className="text-2xl text-white mb-8">{postCategory} posts</div>
