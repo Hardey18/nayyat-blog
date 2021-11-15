7727bfe234c0429f2400da970bb5e4a11eee3c56
Nurudeen Adewale <nurudeenadeolaa@gmail.com>
2021-11-15 15:08:11 +0100

    final clean up

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -8,7 +8,11 @@ const CategoryPost = ({ posts }) => {
   const [postCategory, setPostCategory] = useState("");
   let reversedData = [...posts].reverse();
   useEffect(() => {
-    setPostCategory(posts[0].node.categories[0].name)
+    if (posts[0].node.categories.length > 1) {
+      setPostCategory(posts[0].node.categories[posts[0].node.categories.length - 1].name)
+    } else {
+      setPostCategory(posts[0].node.categories[0].name)
+    }
   })
   const router = useRouter();
 
@@ -21,7 +25,7 @@ const CategoryPost = ({ posts }) => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
         <div className="text-2xl text-white mb-8">{postCategory} posts</div>
-          {reversedData.map((post, index) => (
+          {reversedData?.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
         </div>
