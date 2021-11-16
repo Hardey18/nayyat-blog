f8f4723fbf1403a97a6eeb25d0508fead00c968a
Nurudeen Adewale <nurudeenadeolaa@gmail.com>
2021-11-16 23:21:05 +0100

    revert changes

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -9,8 +9,6 @@ import { getSimilarPosts, getRecentPosts } from '../services';
 const PostWidget = ({ categories, slug }) => {
   const [relatedPosts, setRelatedPosts] = useState([]);
 
-  const reversedPost = [...relatedPosts].reverse();
-
   useEffect(() => {
     if (slug) {
       getSimilarPosts(categories, slug).then((result) => {
@@ -26,7 +24,7 @@ const PostWidget = ({ categories, slug }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
-      {reversedPost.map((post, index) => (
+      {relatedPosts.map((post, index) => (
         <div key={index} className="flex items-center w-full mb-4">
           <div className="w-16 flex-none">
             <Image
